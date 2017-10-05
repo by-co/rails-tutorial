627b605fdb66944dac19d4440e73e13980c88f5d
Barbara Yang <by@berkeley.edu>
2017-10-05 15:09:26 -0700

    5.0 up and running

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # map requests to http://localhost:3000/welcome/index => welcome controller's index action
   get 'welcome/index'
+
+  resources :articles
   # map requests for root => the welcome controller's index action
   root 'welcome#index'
 end
