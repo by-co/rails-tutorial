f63ff91d4337825c973bc4573c08f497f348ed2a
Barbara Yang <by@berkeley.edu>
2017-10-05 18:19:45 -0700

    6.2 associating models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  has_many :comments
   # ensures that all titles are >5 characters long
   validates :title, presence: true,
                     length: { minimum: 5 }
