b1816a279f394c1713f38310e575b646fed97a6e
Barbara Yang <by@berkeley.edu>
2017-10-05 18:20:26 -0700

    6.3 adding a route for comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # map requests to http://localhost:3000/welcome/index => welcome controller's index action
   get 'welcome/index'
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # map requests for root => the welcome controller's index action
   root 'welcome#index'
 end
