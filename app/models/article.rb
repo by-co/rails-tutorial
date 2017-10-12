b116de83683e582c0cb5f591329a28e7f92bdb41
Barbara Yang <by@berkeley.edu>
2017-10-12 14:27:11 -0700

    8.1 deleting associated objects

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
   # ensures that all titles are >5 characters long
   validates :title, presence: true,
                     length: { minimum: 5 }
